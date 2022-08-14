d6ca129edb77d446113cf14dd787cc80494f9c05
KoutarouY <kou06yokohama@gmail.com>
2022-08-14 17:10:25 +0900

    Revert "s"
    This reverts commit feef7092aff9bf536c01f7b96df7d094fa0486ab.

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,26 @@
 <template>
-  <div>
-    <h1>Gitの紹介</h1>
-
-    <h2>チーム開発の流れ</h2>
-
-    <h3>
-      1.リモートリポジトリの状態をローカルリポジトリのmasterブランチに取り入れ、最新の状態にする
-    </h3>
-    <ol>
-      <li>git chekcout master</li>
-      <li>git fetch</li>
-      <li>git merge origin/master</li>
-    </ol>
-    <h3>2.ブランチを切って開発をする</h3>
-    <ol>
-      <li>git branchでブランチを作成</li>
-      <li>git checkout ブランチ名 <br />開発し終えたら...</li>
-      <li>git commit -m "コミットメッセージ"</li>
-      <li>git push origin ブランチ名</li>
-    </ol>
-    <h3>3.Pull requestを作成する</h3>
-    <ol>
-      <li>GitHub で、Compare & pull requestをクリック</li>
-      <li>必要事項を記入し、Create pull request</li>
-    </ol>
-    <h3>4.開発した人以外の人がレビューしてマージする</h3>
-    <ol>
-      <li>
-        Pull requestをレビューし、問題なかったらMerge pull requestでマージする
-      </li>
-    </ol>
-    <h4>これを繰り返す！</h4>
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-export default {};
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
 </script>
 
-<style></style>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
